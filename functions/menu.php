d41e71ce64016a2176f8665ca9ec556c6fbb4504
Daniel Blair <CMDann.Development@gmail.com>
2014-07-18 11:37:01 -0500

    added new zip and fixed broken menu

--- functions/menu.php
+++ functions/menu.php
@@ -6,11 +6,11 @@ function pano_create_menu() {
 	create_sub_menus();
 }
 
-// function to create the top level menus
+// function to create the top level meny
 function create_top_menu(){
-	add_menu_page('Panos', 
+	add_menu_page('pano-settings', 
 				  'Panos', 
-				  'pano-settings', 
+				  'administrator', 
 				  __FILE__, 
 				  'pano_settings_page',
 				  plugins_url('/images/icon.png', 
@@ -22,7 +22,7 @@ function create_top_menu(){
 function create_sub_menus(){
 	add_submenu_page("pano-settings", 
 					 "Quests", 
-					 "Quests", 
+					 "administrator", 
 					 0, 
 					 "pano_quest_settings", 
 					 "pano_quest_settings_page");
