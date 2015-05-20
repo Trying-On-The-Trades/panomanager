be072d5f55e6c2d26e2ae7de229ab76459b66f86
Douglas Modena <d.modena@live.com>
2015-05-20 11:06:50 -0500

    Moved oppia to its own function.

--- js/featherlight/featherlight-functions.js
+++ js/featherlight/featherlight-functions.js
@@ -13,6 +13,67 @@
 */
 
 /*
+  Opens a pop-up with html content using ajax.
+  Parameters:
+  - htm (Ajax path)
+  Returns: void
+*/
+function loadAjax(htm){
+  $.featherlight(htm, {type: 'ajax'});
+}
+
+/*
+  Opens a pop-up with a frame.
+  Widht and height can be specified, or they will use YouTube standard embedded video size (560 x 315).
+  If you want to save your activity points, set the pts parameter to true.
+  Parameters:
+  - frm (Frame address)
+  - width (Pop-up width) [Default value: 560]
+  - height (Pop-up height) [Default value: 315]
+  - pts (Save points) [Default value: false]
+*/
+function loadFrame(frm, width, height, pts){
+  // Standard width: 560
+  if(width == null){
+    width = 560;
+  }
+  // Standard height: 315
+  if(height == null){
+    height = 315;
+  }
+  // Standard pts: false
+  if(pts == null){
+    pts = false;
+  }
+
+  // Loading frame without pts
+  if(!pts){
+    $.featherlight({iframe: frm, iframeWidth: width, iframeHeight: height});
+  }
+
+  // Loading frame with pts
+  if(pts){
+    // Variable to store points achieved
+    var points = 0;
+
+    // Getting points value
+    getPts = function(){
+      var iframe = document.getElementsByTagName('iframe')[1];
+      var innerDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document;
+      window.points = innerDoc.getElementById('points').getAttribute('value');
+    }
+    // Adding points to db and toast
+    showPts = function(){
+      if(window.points > 0){
+        addPts(4, points);
+      }
+    }
+
+    $.featherlight({iframe: frm, iframeWidth: width, iframeHeight: height, beforeClose: getPts, afterClose: showPts});
+  }
+}
+
+/*
   Opens a pop-up with an image.
   Parameters:
   - img (Image path)
@@ -23,57 +84,48 @@ function loadImage(img){
 }
 
 /*
- Opens a pop-up with a frame.
- Width and height can be specified, or they will use Youtube standard embedded video size (560 x 315)
- The last argument is optional, and used when loading an Google Oppia game.
- Parameters:
- - frm (Frame address)
- - width (Pop-up width) [Default value: 560]
- - height (Pop-up height) [Default value: 315]
- - oppia (Unique oppia game identifier, or oppia-id)
- Returns: void
+  Opens a pop-up with an Oppia Exploration.
+  If you want to save your activity points, set the pts parameter to true.
+  Parameters:
+  - frm (Frame address)
+  - id (Oppia unique identifier)
+  - width (Pop-up width)
+  - height (Pop-up height)
+  - pts (Save points) [Default value: false]
 */
-function loadFrame(frm, width, height, oppia){
-  // Standard width
-  if(width == null){
-    width = 560;
+function loadOppia(frm, id, width, height, pts){
+  // Saving oppia id
+  aux = '?oppia=' + id;
+  id = aux;
+
+  // Standard pts: false
+  if(pts == null){
+    pts = false;
   }
-  // Standard height
-  if(height == null){
-    height = 315;
+
+  // Loading frame without pts
+  if(!pts){
+    $.featherlight({iframe: frm + id, iframeWidth: width, iframeHeight: height});
   }
-  // Loading non-oppia iframe
-  if(oppia == null){
-    $.featherlight({iframe: frm, iframeWidth: width, iframeHeight: height});
-  }
-  // Loading oppia iframe
-  if(oppia != null){
-    var aux = '?oppia=' + oppia;
-    oppia = aux;
+
+  // Loading frame with pts
+  if(pts){
+    // Variable to store points achieved
     var points = 0;
 
-    function getPts(){
+    // Getting points value
+    getPts = function(){
       var iframe = document.getElementsByTagName('iframe')[1];
       var innerDoc = (iframe.contentDocument) ? iframe.contentDocument : iframe.contentWindow.document;
       window.points = innerDoc.getElementById('points').getAttribute('value');
-      console.log(window.points);
     }
-
+    // Adding points to db and toast
     showPts = function(){
       if(window.points > 0){
         addPts(4, points);
       }
     }
-    $.featherlight({iframe: frm + oppia, iframeWidth: width, iframeHeight: height, beforeClose: getPts, afterClose: showPts});
-  }  
-}
 
-/*
-  Opens a pop-up with html content using ajax
-  Parameters:
-  - htm (Ajax path)
-  Returns: void
-*/
-function loadAjax(htm){
-  $.featherlight(htm, {type: 'ajax'});
+    $.featherlight({iframe: frm + id, iframeWidth: width, iframeHeight: height, beforeClose: getPts, afterClose: showPts});
+  }
 }
