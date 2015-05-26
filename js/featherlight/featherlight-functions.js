6b12729c7c8452877db570fdcf963ad1fd6ddfd3
Douglas Modena <d.modena@live.com>
2015-05-26 14:39:04 -0500

    Documentation to new functions to db.

--- js/featherlight/featherlight-functions.js
+++ js/featherlight/featherlight-functions.js
@@ -18,27 +18,81 @@
   - id (activity id)
   - pts (number of points to be added)
 */
-function addPointsFeather(act_id, pts){
-  // Checking if activity was previously done
-  var done = false;
-  var done_activities = $('#done_activities').text();
-  acts = done_activities.split(',');
-  for(var i = 0; i < acts.length; i++){
-    if(act_id == acts[i]){
-      done = true;
-    }
+
+// Old function
+// function addPointsFeather(act_id, pts){
+//   // Checking if activity was previously done
+//   var done = false;
+//   var done_activities = $('#done_activities').text();
+//   acts = done_activities.split(',');
+//   for(var i = 0; i < acts.length; i++){
+//     if(act_id == acts[i]){
+//       done = true;
+//     }
+//   }
+//   if(!done){
+//     // Checking for positive number of points
+//     if(pts > 0){
+//       var totalPoints = $('#bonus_points').text();
+//       totalPoints = parseInt(totalPoints, 10);
+//       totalPoints = totalPoints + parseInt(pts, 10);
+//       $('#bonus_points').html(totalPoints);
+//       $('#done_activities').html(done_activities + act_id.toString() + ',');
+//       $().toastmessage('showSuccessToast', 'You earned ' + pts + ' points!');
+//     }
+//   }
+// }
+
+/*
+  Adds bonus points from activities to database.
+  Parameters:
+  - hot_id (Hotspot id)
+  - pts (Number of bonus points)
+*/
+function addBnsPtsFeather(hot_id, pts){
+  if(pts > 0){
+    var postUrl = document.getElementById('app_css-css').getAttribute('href').split('wordpress')[0]+'wordpress/wp-admin/admin-post.php';
+    $.ajax({
+      type: 'POST',
+      url: postUrl,
+      data: {
+        action: 'update_progress_with_bonus',
+        hotspot: hot_id,
+        bonus_points: pts
+      },
+      success: function(){
+        var totalPoints = $('#bonus_points').text();
+        totalPoints = parseInt(totalPoints, 10);
+        totalPoints = totalPoints + parseInt(pts, 10);
+        $('#bonus_points').html(totalPoints);
+        $('#done_activities').html(done_activities + hot_id.toString() + ',');
+        $().toastmessage('showSuccessToast', 'You earned ' + pts + ' points!');
+      }
+    });
   }
-  if(!done){
-    // Checking for positive number of points
-    if(pts > 0){
-      var totalPoints = $('#bonus_points').text();
-      totalPoints = parseInt(totalPoints, 10);
-      totalPoints = totalPoints + parseInt(pts, 10);
-      $('#bonus_points').html(totalPoints);
-      $('#done_activities').html(done_activities + act_id.toString() + ',');
-      $().toastmessage('showSuccessToast', 'You earned ' + pts + ' points!');
+}
+
+/*
+  Adds points from missions to database. The number of points is already on the database.
+  Parameter:
+  - hot_id (Hotspot id)
+*/
+function addRegPtsFeather(hot_id){
+  var postUrl = document.getElementById('app_css-css').getAttribute('href').split('wordpress')[0]+'wordpress/wp-admin/admin-post.php';
+  $.ajax({
+    type: 'POST',
+    url: postUrl,
+    data: {
+      action: 'update_progress',
+      hotspot: hot_id
+    },
+    success: function(){
+      var hotspot = document.getElementById(hot_id+'_menu_item');
+      hotspot.setAttribute('class', 'hotspot_done');
+      var hotspotPoints = parseInt(hotspot.getElementsByClassName('hotspot_points')[0].innerHTML);
+      $().toastmessage('showSuccessToast', 'You earned ' + hotspotPoints + ' points!');
     }
-  }
+  });
 }
 
 /*
@@ -68,7 +122,12 @@ function loadFrame(act_id, frm, pts){
 
   // Loading frame without pts
   if(!pts){
-    $.featherlight({iframe: frm, iframeWidth: width, iframeHeight: height});
+
+    // Adding points to db and toast
+    showPts = function(){
+        addRegPtsFeather(act_id);
+    }
+    $.featherlight({iframe: frm, iframeWidth: width, iframeHeight: height, afterClose: showPts});
   }
 
   // Loading frame with pts
@@ -86,7 +145,7 @@ function loadFrame(act_id, frm, pts){
     // Adding points to db and toast
     showPts = function(){
       if(fpoints > 0){
-        addRegPtsFeather(act_id, fpoints);
+        addBnsPtsFeather(act_id, fpoints);
       }
     }
 
@@ -136,47 +195,4 @@ function loadOppia(act_id, frm, oppia_id, award_points, base_points, timer, bonu
     }
   }
   loadFrame(act_id, frame_address, award_points);
-}
-
-
-function addBnsPtsFeather(hot_id, pts){
-  if(pts > 0){
-    var postUrl = document.getElementById('app_css-css').getAttribute('href').split('wordpress')[0]+'wordpress/wp-admin/admin-post.php';
-    $.ajax({
-      type: 'POST',
-      url: postUrl,
-      data: {
-        action: 'update_progress_with_bonus',
-        hotspot: hot_id,
-        bonus_points: pts
-      },
-      success: function(){
-        var totalPoints = $('#bonus_points').text();
-        totalPoints = parseInt(totalPoints, 10);
-        totalPoints = totalPoints + parseInt(pts, 10);
-        $('#bonus_points').html(totalPoints);
-        $('#done_activities').html(done_activities + hot_id.toString() + ',');
-        $().toastmessage('showSuccessToast', 'You earned ' + pts + ' points!');
-      }
-    });
-  }
-}
-
-
-function addRegPtsFeather(hot_id){
-  var postUrl = document.getElementById('app_css-css').getAttribute('href').split('wordpress')[0]+'wordpress/wp-admin/admin-post.php';
-  $.ajax({
-    type: 'POST',
-    url: postUrl,
-    data: {
-      action: 'update_progress',
-      hotspot: hot_id
-    },
-    success: function(){
-      var hotspot = document.getElementById(hot_id+'_menu_item');
-      hotspot.setAttribute('class', 'hotspot_done');
-      var hotspotPoints = parseInt(hotspot.getElementsByClassName('hotspot_points')[0].innerHTML);
-      $().toastmessage('showSuccessToast', 'You earned ' + hotspotPoints + ' points!');
-    }
-  });
 }
