8962a3cec3ac70912a5348574f896087f81f86a4
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2015-05-27 13:32:58 -0500

    Changed to domain in admin files part 2

--- admin/trades.php
+++ admin/trades.php
@@ -1,12 +1,12 @@
 <?php
 
 // Build the settings page
-function pano_trade_settings_page() {
-    $trades = get_trades();
+function pano_domain_settings_page() {
+    $domains = get_domains();
 
     $semantic       = WP_PLUGIN_URL . '/panomanager/css/semantic.css';
-    $new_trade_url  = admin_url() . "admin.php?page=new_trade_settings";
-    $edit_trade_url = admin_url() . "admin.php?page=edit_trade_settings";
+    $new_domain_url  = admin_url() . "admin.php?page=new_domain_settings";
+    $edit_domain_url = admin_url() . "admin.php?page=edit_domain_settings";
     ?>
 
 <!-- style sheet so our admin page looks nice -->
@@ -18,23 +18,23 @@ function pano_trade_settings_page() {
     <div class="updated"><p>Settings updated successfully.</p></div>
 <?php endif ?>
 
-<h2>Trades</h2>
+<h2>Domains</h2>
 <table class="ui table segment">
   <tr>
-    <th>Trade</th>
+    <th>Domain</th>
     <th>Edit</th>
     <th>Delete</th>
   </tr>
-  <?php foreach ($trades as $trade): ?>
+  <?php foreach ($domains as $domain): ?>
 
     <tr>
-        <td><?php echo $trade->name ?></td>
-        <td><a class="ui blue icon button" href="<?php echo $edit_trade_url ?>&id=<?php echo $trade->id ?>" style="padding: 7px">Edit</a></td>
+        <td><?php echo $domain->name ?></td>
+        <td><a class="ui blue icon button" href="<?php echo $edit_domain_url ?>&id=<?php echo $domain->id ?>" style="padding: 7px">Edit</a></td>
         <td>
-            <form method="post" action="admin-post.php" id="delete_trade_form<?php echo $trade->id ?>">
+            <form method="post" action="admin-post.php" id="delete_domain_form<?php echo $domain->id ?>">
                 <!-- pano processing hook -->
-                <input type="hidden" name="action" value="delete_trade" />
-                <input type="hidden" name="trade_id" value="<?php echo $trade->id ?>" />
+                <input type="hidden" name="action" value="delete_domain" />
+                <input type="hidden" name="domain_id" value="<?php echo $domain->id ?>" />
 
                 <input type="submit" class="ui blue icon button" value="Delete" style="padding: 7px" >
             </form>
@@ -43,7 +43,7 @@ function pano_trade_settings_page() {
 
 <?php endforeach; ?>
 </table>
-<a class="ui blue icon button" href="<?php echo $new_trade_url ?>" style="padding: 7px">New Trade</a>
+<a class="ui blue icon button" href="<?php echo $new_domain_url ?>" style="padding: 7px">New Domain</a>
 </div>
 
 <?php }
