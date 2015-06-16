fd1a08e71a3792998dfc74e219b82b99c9613617
Douglas Modena <d.modena@live.com>
2015-06-16 14:20:06 -0500

    Fixed both triggers.

--- functions/db_triggers.php
+++ functions/db_triggers.php
@@ -2,19 +2,25 @@
 
 function trigger_points_to_wallet(){
   global $wpdb;
+  $hotspost_table_name = get_hotspot_table_name();
   $points_table_name = get_user_skill_progress_table_name();
+  $wallet_table_name = get_wallet_table_name();
 
-  $sql = "CREATE OR REPLACE TRIGGER PointsWallet
+  $sql = "DELIMITER //
+  CREATE OR REPLACE TRIGGER PointsWallet
   AFTER INSERT ON $points_table_name
   FOR EACH ROW
   BEGIN
-    UPDATE Products
-    SET QtyOnHand = QtyOnHand - :NEW.Qty
-    WHERE(Mfr = :NEW.Mfr
-      AND Product = :NEW.Product);
+    DECLARE points AS INT(10);
+    SELECT points INTO points FROM $hotspost_table_name
+      WHERE id = :NEW.skill_id;
+    UPDATE $wallet_table_name
+    SET available_currency = available_currency + points
+    WHERE(user_id = :NEW.user_id);
   END;
-  /";
+  DELIMITER ;";
 
+  return $sql;
 }
 
 function trigger_bonus_points_to_wallet(){
@@ -22,15 +28,16 @@ function trigger_bonus_points_to_wallet(){
   $bonus_points_table_name = get_user_skill_bonus_pts_table_name();
   $wallet_table_name = get_wallet_table_name();
 
-  $sql = "CREATE OR REPLACE TRIGGER BonusPointsWallet
+  $sql = "DELIMITER //
+  CREATE OR REPLACE TRIGGER BonusPointsWallet
   AFTER INSERT ON $bonus_points_table_name
   FOR EACH ROW
   BEGIN
     UPDATE $wallet_table_name
     SET available_currency = available_currency + :NEW.bonus_points
-    WHERE(user_id = :NEW.user_id);
+    WHERE (user_id = :NEW.user_id);
   END;
-  /";
+  DELIMITER ;";
 
   return $sql;
 }
