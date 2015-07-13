2099fe379a033f7079c2b7fcf2f10ece413420d2
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2015-07-13 14:29:08 -0500

    Base page for purchase

--- shop/shop.php
+++ shop/shop.php
@@ -1,9 +1,31 @@
 <?php 
+	require('db.php');
+
 	if(isset($_POST[''])){
 		//create a purchase
 	}
 
 	if(isset($_GET['id'])){
 		//search for an item
+		$item;
+		$symbol;
+		$path = '../../../';
 	}
 ?>
+<!DOCTYPE HTML>
+<html>
+	<head></head>
+	<body>
+		<div class="content">
+			<img src="<?= $path . $item['image'] ?>" alt="Image">
+			<h4><?= $item['name'] ?></h4>
+			<p><?= $item['description'] ?></p>
+			<p><?= $symbol . $item['price'] ?></p>
+			<form method="post">
+				<input type="hidden" name="item" value="<?= $item['id'] ?> "/>
+				<input type="hidden" name="price" value="<?= $item['price'] ?>" />
+				<button type="submit">Buy it</button>
+			</form>
+		</div>
+	</body>
+</html>
