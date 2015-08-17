ed538be3e1f5e09441135cd80e071fc9aa2eb584
Andre F. C. Silva <andrefelipe.costa@hotmail.com>
2015-08-17 11:46:16 -0500

    Added filtering by hotspot type on the hotspots table

--- admin/hotspots.php
+++ admin/hotspots.php
@@ -4,6 +4,7 @@
 function pano_hotspot_settings_page() {
     $hotspots = get_hotspots();
     $panos = get_panos();
+    $types = get_types();
 
     $semantic         = WP_PLUGIN_URL . '/panomanager/css/semantic.css';
     $hotspot_types    = admin_url() . "admin.php?page=pano_hotspot_type_settings";
@@ -23,11 +24,20 @@ function pano_hotspot_settings_page() {
 
 <div>
     <h2>Hotspots</h2>
-    <label>Filter by Pano: </label>
+    <p>Filter hotspots by:</p>
+    <label>Pano </label>
     <select id="pano_select">
         <option value="NA">Select a pano</option>
         <?php foreach($panos as $pano): ?>
             <option value="<?= $pano->id ?>"><?= $pano->title ?></option>
+        <?php endforeach; ?>
+    </select>
+
+    <label>Hotspot Type </label>
+    <select id="type_select">
+        <option value="NA">Select a type</option>
+        <?php foreach($types as $type): ?>
+            <option value="<?= $type->id ?>"><?= $type->name ?></option>
         <?php endforeach; ?>
     </select>
 </div>
@@ -49,7 +59,7 @@ function pano_hotspot_settings_page() {
     <tbody>
         <?php foreach ($hotspots as $hotspot): ?>
             <?php $current_hotspot = build_hotspot($hotspot->id); ?>
-            <tr class="hotspot pano<?= $hotspot->pano_id ?>">
+            <tr class="hotspot pano<?= $hotspot->pano_id ?> type<?= $hotspot->type_id ?>">
                 <td><?php echo get_pano($hotspot->pano_id)->title; ?></td>
                 <td><?php echo $current_hotspot->get_name(); ?></td>
                 <td><?php echo $current_hotspot->get_menu_name(); ?></td>
@@ -80,16 +90,35 @@ function pano_hotspot_settings_page() {
 <script>
     jQuery(document).ready(function(){
         jQuery("#hostpotTable").tablesorter();
+
+
         jQuery("#pano_select").change( function() {
+            filter_hotspots();
+        });
+
+        jQuery("#type_select").change( function(){
+            filter_hotspots();
+        });
+
+        function filter_hotspots() {
             var pano = jQuery("#pano_select").prop("value");
-            if(pano != "NA") {
+
+            var type = jQuery("#type_select").prop("value");
+
+
+            if (pano == "NA" && type == "NA") {
+                jQuery(".hotspot").show();
+            } else if (pano == "NA" && type != "NA") {
+                jQuery(".hotspot").hide();
+                jQuery(".type" + type).show();
+            } else if (pano != "NA" && type == "NA") {
                 jQuery(".hotspot").hide();
                 jQuery(".pano" + pano).show();
             } else {
-                jQuery(".hotspot").show();
+                jQuery(".hotspot").hide();
+                jQuery(".pano" + pano + ".type" + type).show();
             }
-
-        });
+        }
     })
 </script>
 
